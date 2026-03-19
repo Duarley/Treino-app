69d676822fe11697b54c57cd36e58cdbedd6b13b
root <root@localhost.localdomain>
2026-03-19 01:52:49 +0000

    fix: remove manual plugin registration

--- android/app/src/main/java/com/treino/duassemanas/MainActivity.java
+++ android/app/src/main/java/com/treino/duassemanas/MainActivity.java
@@ -1,13 +1,5 @@
 package com.treino.duassemanas;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitorcommunity.texttospeech.TextToSpeechPlugin;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    registerPlugin(TextToSpeechPlugin.class);
-  }
-}
+public class MainActivity extends BridgeActivity {}
